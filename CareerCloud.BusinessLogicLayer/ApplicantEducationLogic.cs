c8c1c908340f5d0072c0530c8c91a75d5d03b735
rsn133 <rsn133@gmail.com>
2018-07-09 14:55:07 -0400

    Business Logic Objects

---
+++ CareerCloud.BusinessLogicLayer/ApplicantEducationLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CareerCloud.DataAccessLayer;
+using CareerCloud.Pocos;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantEducationLogic:BaseLogic<ApplicantEducationPoco>
+    {
+        public ApplicantEducationLogic(IDataRepository<ApplicantEducationPoco> repository):base(repository)
+        {
+
+        }
+    }
+}
